78cd1bd9add9a9cea1859afb902d85b1664233da
RominaMaita <u202213765@upc.edu.pe>
2025-05-08 12:17:24 -0500

    fix: cleaning non used components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,13 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
-import {TranslatePipe, TranslateService} from '@ngx-translate/core';
-import {
-  LanguageSwitcherComponentComponent
-} from './public/components/language-switcher.component/language-switcher.component.component';
-import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
-import {MatAnchor} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbar, MatToolbarRow, MatAnchor, RouterLink, RouterLinkActive, TranslatePipe, LanguageSwitcherComponentComponent],
+  imports: [],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'KeepItFresh';
 
-  options =[
-    { link: '/home', label: 'home'}
-  ]
-
-  constructor(private translate: TranslateService){
-    this.translate.setDefaultLang('en');
-    this.translate.use('en');
-  }
 }
