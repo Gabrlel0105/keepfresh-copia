584738ec8b9e8b5989471fef692337924f77456f
RominaMaita <u202213765@upc.edu.pe>
2025-05-08 20:45:10 -0500

    feat(main): add dependencies.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {OrderFormComponent} from './order/components/order-form/order-form.component';
+import {OrderListComponent} from './order/components/order-list/order-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    OrderFormComponent,
+    OrderListComponent
+  ],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
