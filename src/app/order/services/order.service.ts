9f6f4120e23f85bcf0896b42dbd93bb949f05c50
RominaMaita <u202213765@upc.edu.pe>
2025-05-08 20:46:24 -0500

    feat(order): add order service logic.

--- src/app/order/services/order.service.ts
+++ src/app/order/services/order.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {BaseService} from '../../shared/services/base.service';
 import {Order} from '../models/order.entity';
+import {Observable} from 'rxjs';
 import {environment} from '../../../environments/environment';
 
 const orderResourceEndpointPath = environment.ordersEndpointPath;
@@ -8,12 +9,24 @@ const orderResourceEndpointPath = environment.ordersEndpointPath;
 @Injectable({
   providedIn: 'root'
 })
+
 export class OrderService extends BaseService<Order>{
+
+  //const orderResourceEndpointPath = environment.ordersEndpointPath;
 
   constructor() {
     super();
     this.resourceEndpoint = orderResourceEndpointPath;
   }
 
+  protected override   = '/orders';
+
+  removeOrder(id: number): Observable<void> {
+    return this.delete(id);
+  }
+
+  getOrders(): Observable<Order[]> {
+    return this.getAll();
+  }
 
 }
