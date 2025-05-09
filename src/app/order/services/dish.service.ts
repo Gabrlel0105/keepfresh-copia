7d9bf01d5487e543cc383c0b3e2279e8b414faa6
RominaMaita <u202213765@upc.edu.pe>
2025-05-08 22:20:08 -0500

    fix: changed logic of services

--- src/app/order/services/dish.service.ts
+++ src/app/order/services/dish.service.ts
@@ -1,18 +1,13 @@
 import { Injectable } from '@angular/core';
-import {BaseService} from '../../shared/services/base.service';
 import {Dish} from '../models/dish.entity';
 import {environment} from '../../../environments/environment';
+import {BaseService} from '../../shared/services/base.service';
 
-const dishesResourceEndpointPath = environment.dishesEndpointPathsEndpointPath;
 @Injectable({
   providedIn: 'root'
 })
 
 
 export class DishService extends BaseService<Dish>{
-
-  constructor() {
-    super();
-    this.resourceEndpoint = dishesResourceEndpointPath;
-  }
+  protected override resourceEndpoint = environment.dishesEndpointPath;
 }
--- src/app/order/services/order.service.ts
+++ src/app/order/services/order.service.ts
@@ -1,10 +1,7 @@
 import { Injectable } from '@angular/core';
 import {BaseService} from '../../shared/services/base.service';
 import {Order} from '../models/order.entity';
-import {Observable} from 'rxjs';
 import {environment} from '../../../environments/environment';
-
-const orderResourceEndpointPath = environment.ordersEndpointPath;
 
 @Injectable({
   providedIn: 'root'
@@ -12,21 +9,6 @@ const orderResourceEndpointPath = environment.ordersEndpointPath;
 
 export class OrderService extends BaseService<Order>{
 
-  //const orderResourceEndpointPath = environment.ordersEndpointPath;
-
-  constructor() {
-    super();
-    this.resourceEndpoint = orderResourceEndpointPath;
-  }
-
-  protected override   = '/orders';
-
-  removeOrder(id: number): Observable<void> {
-    return this.delete(id);
-  }
-
-  getOrders(): Observable<Order[]> {
-    return this.getAll();
-  }
+  protected override resourceEndpoint = environment.ordersEndpointPath;
 
 }
