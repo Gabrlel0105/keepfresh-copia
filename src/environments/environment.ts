468d5d793757fc071255122427ad7fd66d0b3905
RominaMaita <u202213765@upc.edu.pe>
2025-05-08 20:45:25 -0500

    feat(env): add environments.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   serverBaseUrl: 'http://localhost:3000/api/v1',
   ordersEndpointPath: '/orders',
+  dishesEndpointPathsEndpointPath: '/dishes',
 };
