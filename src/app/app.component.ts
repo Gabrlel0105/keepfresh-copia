95c49a8211b35f29dda9f652ebdc8d7b66ecbeb3
RominaMaita <u202213765@upc.edu.pe>
2025-05-08 22:21:27 -0500

    fix: changed dependencies.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import {OrderFormComponent} from './order/components/order-form/order-form.component';
-import {OrderListComponent} from './order/components/order-list/order-list.component';
+
 
 @Component({
   selector: 'app-root',
   imports: [
     OrderFormComponent,
-    OrderListComponent
+
   ],
   templateUrl: './app.component.html',
   standalone: true,
