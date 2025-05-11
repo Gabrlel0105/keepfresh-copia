b8b377222112a2942c20624b353548d885c6bb38
Werner Lang <u202310003@upc.edu.pe>
2025-05-11 17:45:45 -0500

    feat(notification): add service logic and environment variables.

--- src/app/notification/services/notification.service.ts
+++ src/app/notification/services/notification.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getNotifications(){
+    return this.http.get<Notification[]>(environment.serverBaseUrlNotification);
+  }
+
+  addNotification(notification: Notification): Observable<Notification> {
+    return this.http.post<Notification>(environment.serverBaseUrlNotification, notification);
+  }
 }
