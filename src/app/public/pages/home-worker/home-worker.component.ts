aa7e7e96ad11bfbeeb81dc30b4f3d952bd279385
Werner Lang <u202310003@upc.edu.pe>
2025-05-11 16:15:47 -0500

    feat(notification): add home-worker component

---
+++ src/app/public/pages/home-worker/home-worker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-worker',
+  imports: [],
+  templateUrl: './home-worker.component.html',
+  styleUrl: './home-worker.component.css'
+})
+export class HomeWorkerComponent {
+
+}
