a3654f561d2b4e7b8a76be54ae51f598a387a3fb
Werner Lang <u202310003@upc.edu.pe>
2025-05-11 22:40:23 -0500

    feat(notification): entity and service updated. Add base logic for list

--- src/app/notification/components/notification-list/notification-list.component.ts
+++ src/app/notification/components/notification-list/notification-list.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Notification } from '../../models/notification.entity';
+import { NotificationService } from '../../services/notification.service';
 
 @Component({
   selector: 'app-notification-list',
   imports: [],
   templateUrl: './notification-list.component.html',
-  styleUrl: './notification-list.component.css'
+  styleUrls: ['./notification-list.component.css']
 })
-export class NotificationListComponent {
+export class NotificationListComponent implements OnInit {
+  notifications: Notification[] = [];
+  showForm = false;
 
+  newNotificationAdded: Notification = {
+    id: 0,
+    title: '',
+    description: '',
+    type: ''
+  };
+
+  constructor(private notificationService: NotificationService) {}
+
+  toggleForm() {
+    this.showForm = !this.showForm;
+  }
+
+  ngOnInit() {
+    this.loadNotifications();
+  }
+
+  loadNotifications() {
+    this.notificationService.getNotifications().subscribe((data: Notification[]) => {
+      this.notifications = data;
+    });
+  }
 }
--- src/app/notification/models/notification.entity.ts
+++ src/app/notification/models/notification.entity.ts
@@ -4,7 +4,7 @@ export class Notification{
   description?: string;
   type?: string;
 
-  constructor(id: number, title: string, description: string, type: string) {
+  constructor(id?: number, title?: string , description?: string, type?: string) {
     this.id = id;
     this.title = title;
     this.description = description;
